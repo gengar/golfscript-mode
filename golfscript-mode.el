620e044423293550565ef29d0e0f6b43c5e49951
kaki <gengar@users.noreply.github.com>
2023-07-18 13:10:11 +0900

    v0.1.2

--- golfscript-mode.el
+++ golfscript-mode.el
@@ -5,7 +5,7 @@
 ;; Author: 2011-2023 kaki
 ;; Package-Requires: ((cl-lib "1.0") (rx))
 ;; Created: 8 Sep 2011
-;; Version: 0.1.1
+;; Version: 0.1.2
 ;; Keywords: languages, GolfScript
 
 ;; This program is free software: you can redistribute it and/or modify
@@ -27,7 +27,7 @@
   (require 'cl-lib)
   (require 'rx))
 
-(defconst golfscript-mode-version "0.1.1")
+(defconst golfscript-mode-version "0.1.2")
 
 (defface golfscript-mode-assignment-face
   '((t :inherit font-lock-keyword-face))
